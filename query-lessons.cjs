c1f753bed6a43229ef84c871792c82ac905f6277
johan gutierrez <johangutierrez@outlook.cl>
2026-05-20 18:57:39 -0400

    feat: eliminar archivos obsoletos relacionados con la extracción y limpieza de datos

--- query-lessons.cjs
+++
@@ -1,4 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('local.db');
-const lessons = db.prepare('SELECT id, title, `order` FROM lessons ORDER BY `order` ASC LIMIT 30').all();
-console.table(lessons);
